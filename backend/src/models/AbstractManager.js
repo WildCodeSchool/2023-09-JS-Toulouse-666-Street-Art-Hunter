6dfe248035f0c7a19fa5993a45223406fc916ca0
ArthurHtbk <arthur.heurtebise@gmail.com>
2023-12-21 12:15:20 +0100

    test: branch name validation

--- backend/src/models/AbstractManager.js
+++ backend/src/models/AbstractManager.js
@@ -1,7 +1,7 @@
 // Import database client
 const database = require("../../database/client");
 
-// Provide database access through AbstractManager classssss
+// Provide database access through AbstractManager class
 class AbstractManager {
   constructor({ table }) {
     // Store the table name
