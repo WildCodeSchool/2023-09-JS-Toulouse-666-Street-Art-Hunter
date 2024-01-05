390982a50071dea546ee6ccbdca6483b02ca8480
Adri-bis <adrien.ma.16@hotmail.fr>
2024-01-05 11:48:21 +0100

    feat: add some middlewars and change the head of the site

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,6 +7,11 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Importer les modules :
+const validateArtist = require("./middlewares/validateArtist");
+const validatePhoto = require("./middlewares/validatePhoto");
+const validatearticle = require("./middlewares/validateArticle");
+const validateArtwork = require("./middlewares/validateArtwork");
+const validateAvatarImage = require("./middlewares/validateAvatarImage");
 const artworkControllers = require("./controllers/artworkControllers");
 const avatarImageControllers = require("./controllers/avatarImageControllers");
 const userControllers = require("./controllers/userControllers");
@@ -17,22 +22,22 @@ const artistControllers = require("./controllers/artistControllers");
 // Route for ARTWORKS
 router.get("/artworks", artworkControllers.browse);
 router.get("/artworks/:id", artworkControllers.read);
-router.post("/artworks", artworkControllers.add);
-router.put("/artworks/:id", artworkControllers.edit);
+router.post("/artworks", validateArtwork, artworkControllers.add);
+router.put("/artworks/:id", validateArtwork, artworkControllers.edit);
 router.delete("/artworks/:id", artworkControllers.destroy);
 
 // Route for AVATAR_IMAGE
 router.get("/avatars", avatarImageControllers.browse);
 router.get("/avatars/:id", avatarImageControllers.read);
-router.post("/avatars", avatarImageControllers.add);
-router.put("/avatars/:id", avatarImageControllers.edit);
+router.post("/avatars", validateAvatarImage, avatarImageControllers.add);
+router.put("/avatars/:id", validateAvatarImage, avatarImageControllers.edit);
 router.delete("/avatars/:id", avatarImageControllers.destroy);
 
 // Route for PHOTOS
 router.get("/photos", photoControllers.browse);
 router.get("/photos/:id", photoControllers.read);
-router.post("/photos", photoControllers.add);
-router.put("/photos/:id", photoControllers.edit);
+router.post("/photos", validatePhoto, photoControllers.add);
+router.put("/photos/:id", validatePhoto, photoControllers.edit);
 router.delete("/photos/:id", photoControllers.destroy);
 
 // Route for USERS
@@ -45,15 +50,15 @@ router.delete("/users/:id", userControllers.destroy);
 // Route for ARTICLES
 router.get("/articles", articleControllers.browse);
 router.get("/articles/:id", articleControllers.read);
-router.post("/articles", articleControllers.add);
-router.put("/articles/:id", articleControllers.edit);
+router.post("/articles", validatearticle, articleControllers.add);
+router.put("/articles/:id", validatearticle, articleControllers.edit);
 router.delete("/articles/:id", articleControllers.destroy);
 
 // Route for ARTISTE
 router.get("/artists", artistControllers.browse);
 router.get("/artists/:id", artistControllers.read);
-router.post("/artists", artistControllers.add);
-router.put("/artists/:id", artistControllers.edit);
+router.post("/artists", validateArtist, artistControllers.add);
+router.put("/artists/:id", validateArtist, artistControllers.edit);
 router.delete("/artists/:id", artistControllers.destroy);
 
 /* ************************************************************************* */
