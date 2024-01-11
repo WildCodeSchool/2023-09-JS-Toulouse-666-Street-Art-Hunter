c6577770c66fb30a96735cf95ad588532dd7a5bd
Adri-bis <adrien.ma.16@hotmail.fr>
2024-01-11 11:16:04 +0100

    fix: add comment

--- frontend/src/components/Input-R/Input.jsx
+++ frontend/src/components/Input-R/Input.jsx
@@ -5,6 +5,7 @@ import "./Input.scss";
 function Input({ labelName, type, labelText, maxLength }) {
   const [value, setValue] = useState("");
 
+  // Permet de bloquer l'input si le nombre de caractères tapés dépasse la limite.
   const handleClick = (e) => {
     if (e.target.value.length <= maxLength) {
       setValue(e.target.value);
