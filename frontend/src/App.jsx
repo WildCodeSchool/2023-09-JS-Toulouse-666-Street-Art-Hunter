420c28195fcc185c5f8997cc5d6fe10159d468be
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2024-01-15 12:39:25 +0100

    feat: modify landingPage.jsx, finish css part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,10 @@
 import "./App.css";
-import { useNavigate } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
-  const token = localStorage.getItem("token");
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/login");
-  };
-  if (token) {
-    return (
-      <div>
-        <h1>Hello</h1>
-        <div className="Validate-v">V</div>
-        <button type="submit" onClick={handleClick}>
-          Connexion
-        </button>
-      </div>
-    );
-  }
-
   return (
     <div>
-      <h1>Hello</h1>
-      <div className="Validate-x">X</div>
-      <button type="submit" onClick={handleClick}>
-        Connexion
-      </button>
+      <LandingPage />
     </div>
   );
 }
--- frontend/src/components/RootLayout/RootLayout.jsx
+++ frontend/src/components/RootLayout/RootLayout.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import "./RootLayout.scss";
 
 function RootLayout() {
+  const location = useLocation();
+
   return (
     <div className="root-layout-main-container">
-      <header className="header">
-        <div className="links">
-          <Link to="/">Accueil</Link>
-          <Link to="/map">Carte</Link>
-          <Link to="/register">Inscription</Link>
-          <Link to="/login">Connexion</Link>
-          <Link to="/add-existing-artwork">Ajouter-oeuvre</Link>
-        </div>
-      </header>
+      {location.pathname !== "/" && (
+        <header className="header">
+          <div className="links">
+            <Link to="/">Accueil</Link>
+            <Link to="/map">Carte</Link>
+            <Link to="/register">Inscription</Link>
+            <Link to="/login">Connexion</Link>
+            <Link to="/add-existing-artwork">Ajouter-oeuvre</Link>
+          </div>
+        </header>
+      )}
       <Outlet />
     </div>
   );
